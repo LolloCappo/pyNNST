98405707e890666241637d9adf2361f0adaea949
Lorenzo Capponi <lorenzocapponi@outlook.it>
2020-02-26 14:57:48 +0100

    add req and setup

---
+++ setup.py
@@ -0,0 +1,28 @@
+with open('README.rst', 'r') as f:
+    readme = f.read()
+    
+def parse_requirements(filename):
+    ''' Load requirements from a pip requirements file '''
+    with open(filename, 'r') as fd:
+        lines = []
+        for line in fd:
+            line.strip()
+            if line and not line.startswith("#"):
+                lines.append(line)
+    return lines
+
+requirements = parse_requirements('requirements.txt')
+
+#from distutils.core import setup, Extension
+from setuptools import setup, Extension
+from pysfmov import __version__
+setup(name='pyNNST',
+      version=__version__,
+      author='Lorenzo Capponi,
+      author_email='lorenzocapponi@outlook.it',
+      description='Module for reading ResearchIR SFMOV image sequences',
+      url='https://github.com/LolloCappo/pyNNST',
+      py_modules=['pyNNST'],
+      long_description=readme,
+      install_requires=requirements
+      )
