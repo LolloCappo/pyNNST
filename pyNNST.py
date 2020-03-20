4ca5a2e110a91d06047cd911a4ae62fee1af3f91
Lorenzo Capponi <lorenzocapponi@outlook.it>
2020-03-20 16:42:29 +0100

    adjust output

--- pyNNST.py
+++ pyNNST.py
@@ -2,59 +2,87 @@ __version__ = '0.1'
 
 import numpy as np
 
-def idns(signal, nsec, overlap, f_sample, conf):
+def idns(signal, nsec, overlap, f_sample, confidence = 95):
 
     """
-    Function to determine the non-stationarity of a time series:
+    Function to determine the index of non-stationarity of a time series:
         
     Arguments:
-        signal {numpy array} --  column array of the time series
-        nsec {float} -- time lenght of moving window
-        overlap {float} -- overlap between windows [0 - 1]
-        f_sample {int} -- sampling frequency of the time series
-        conf {float} -- confidence interval coefficent [1.96 for 95% - 2.58 for 99%]
+        signal {numpy array} --  Numpy array of the time series
+        nsec {float} -- Time lenght of moving window
+        overlap {float} -- Overlap between windows [0 - 1]
+        f_sample {int} -- Sampling frequency of the time series
+        confidence {int} -- Confidence: 90% - 95% - 98% - 99% - {Default 95(%)}
     
     Returns:
-        index {float} -- index of non-stationarity [Non-stationary --> 0 ; Stationary --> 100]
-        bns {boolean} --  True = Stationary [1]
-                          False = Non-stationary [0]
+    Dictionary: 
+        index {float} -- Index of non-stationarity [Non-stationary --> 0 ; Stationary --> 100]
+        bns {boolean} -- 0 --> Non-Stationary 
+                         1 --> Stationary
+        ind_up {float} -- Upper limit of stationary [%]
+        ind_dw {float} -- Lower limit of stationary [%]
+        nsec {float} -- Time lenght of moving window
+        overlap {float} -- Overlap between windows [0 - 1]
+        confidence {int} -- Confidence: 90% - 95% - 98% - 99% - {Default 95(%)}
     """
 
+    
+    if confidence == 90:
+        conf = 1.645
+    elif confidence == 95:
+        conf = 1.96
+    elif confidence == 98:
+        conf = 2.326
+    elif confidence == 99:
+        conf = 2.576
+    else:
+        print('Please enter a confidence interval between: 90% - 95% - 98% - 99%')
+        print('Default value of 95% is going to be used')
+        conf = 1.96
+        
+        
+    ## Definition of time-window variables 
     fftp = int(f_sample * nsec)
     lap = int(fftp * overlap)
     L = signal.shape[0]
-    dist = fftp - lap
-
-    stdv = np.std(signal)
+    dist = fftp - lap    
     cls = int(np.floor(L/(dist)))
     cmp = np.zeros([dist,cls])
     m_cmp = np.empty(0)
     stdv_cmp = np.empty(0)
+    
+    ## Definition of statistical properties
+    stdv = np.std(signal)
 
+    ## Division of signal in compartments (+residues)
     for i in range(0,cls):
-        cmp[:,i] = signal[i*dist:i*dist+dist,0]
+        cmp[:,i] = signal[i*dist:i*dist+dist]
         m_cmp = np.append(m_cmp, np.mean(cmp[:,i]))
         stdv_cmp = np.append(stdv_cmp, np.std(cmp[:,i]))
+        
     if L % dist != 0:
         res_cmp = signal[cls*dist+1:]
-        m_cmp = np.append(m_cmp, np.mean(res_cmp))
+        m_cmp = np.append(m_cmp,np.mean(res_cmp))
         stdv_cmp = np.append(stdv_cmp, np.std(res_cmp))
     else:
         pass
 
+    ## Definition of boundaries
     boundUP = stdv + np.std(stdv_cmp)
     boundDW = stdv - np.std(stdv_cmp)
 
+    
+    ## Run-computation using boundaures
     run = np.empty(0)
     pos = np.empty(0)
     neg = np.empty(0)
 
-    for i in range(0, stdv_cmp.shape[0]):
+    for i in range(0,stdv_cmp.shape[0]):
         if stdv_cmp[i] > boundUP or stdv_cmp[i] < boundDW:
-            run = np.append(run, 1)
+            run = np.append(run,1)
         else:
-            run = np.append(run, 0)
-    for i in range(0, run.shape[0]):
+            run = np.append(run,0)
+    for i in range(0,run.shape[0]):
         if run[i] == 1.:
             pos = np.append(pos, run[i])
         else:
@@ -62,32 +90,42 @@ def idns(signal, nsec, overlap, f_sample, conf):
 
     N1 = pos.shape[0]
     N0 = neg.shape[0]
-    N = N1 + N0
     Nr = 0
+    N = N1 + N0
+    
 
     for i in range(1,run.shape[0]):
         if run[i] != run[i-1]:
             Nr += 1
         else:
             continue
-        
-    mean_val = (2 * N1 * N0)/N+1
+    
+    ## Definition of expected runs and of the variance of their distribution 
+    mean_val = (2 * N1 * N0) / N + 1
+    var = (2 * N1 * N0 * (2 * N1 * N0 - N)) / (N**2 * (N - 1))
+    
+    ## Definition of limits
+    lim_up = mean_val + conf * np.sqrt(var)
+    lim_dw = mean_val - conf * np.sqrt(var)
+    
+    ind_up = np.round(100 * lim_up / mean_val, 2)
+    ind_dw = np.round(100 * lim_dw / mean_val, 2)
+    
+    
+    ## True == Stationary & False == Non-stationary
+    if Nr >= lim_dw and Nr <= lim_up:
+        bns = True
+    else:
+        bns = False
 
-    index = 100*Nr/mean_val 
+    ## Index of non-stationary
+    index = 100 * Nr / mean_val 
 
     if index > 100:
         index = 100
     else:
         index = np.round(index,2)
         
-    var = (2 * N1 * N0 * (2 * N1 * N0-N)) / (N**2 * (N-1))
-    INTE1 = mean_val + conf * np.sqrt(var)
-    INTE2 = mean_val - conf * np.sqrt(var)
+    nnst = {'index': index, 'bns':bns, 'ind_up':ind_up, 'ind_dw':ind_dw, 'nsec':nsec, 'overlap':overlap, 'confidence':confidence}
     
-    if Nr >= INTE2 and Nr <= INTE1:
-        bns = True # Stationary signal
-    else:
-        bns = False # Non-stationary signal
-
-    
-    return index, bns
+    return nnst
