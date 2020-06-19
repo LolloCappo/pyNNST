fbdad61b084af93bb3fc6f300038d56a8a349199
Lorenzo Capponi <lorenzocapponi@outlook.it>
2020-06-19 11:10:54 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,6 @@
 with open('README.rst', 'r') as f:
     readme = f.read()
     
-def parse_requirements(filename):
-    ''' Load requirements from a pip requirements file '''
-    with open(filename, 'r') as fd:
-        lines = []
-        for line in fd:
-            line.strip()
-            if line and not line.startswith("#"):
-                lines.append(line)
-    return lines
-
-requirements = parse_requirements('requirements.txt')
 
 #from distutils.core import setup, Extension
 from setuptools import setup, Extension
